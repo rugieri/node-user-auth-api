6f8837db1bc24dcb8888ebe0ac4da3d71c92b442
rugieri <rugieri.ronaldo@gmail.com>
2021-09-22 20:30:34 -0300

    added end-points  to users CRUD and application status

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
-
+import express from 'express';
+import statusRoute from './routes/status-route';
+import usersRoute from './routes/users-route';
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'bar' });
-});
+//App config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//Routes config
+app.use(usersRoute);
+
+app.use(statusRoute);
+
+//Init server
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
