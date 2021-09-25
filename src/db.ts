0401557e6a59a44668c722a2b4d758c9a314aa2a
rugieri <rugieri.ronaldo@gmail.com>
2021-09-25 20:07:45 -0300

    created the basic structure for database connection

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import { Pool } from 'pg';
 
-const connectionString = '';
+const connectionString =
+  'postgres://kdqbbfda:ud3Xuvij7d1TPStdzzEyLh9jWZP-ikG-@motty.db.elephantsql.com/kdqbbfda';
 
 const db = new Pool({ connectionString });
 
---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,9 @@
+import User from '../models/user-model';
+
+class UserRepository {
+  findAllUsers(): User[] {
+    return [];
+  }
+}
+
+export default new UserRepository();
