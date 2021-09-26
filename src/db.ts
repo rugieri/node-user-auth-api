beab06bc053b1803230df1f2e461f4dffdfbcadd
rugieri <rugieri.ronaldo@gmail.com>
2021-09-25 23:00:19 -0300

    fixed leak

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString =
-  'postgres://kdqbbfda:ud3Xuvij7d1TPStdzzEyLh9jWZP-ikG-@motty.db.elephantsql.com/kdqbbfda';
+const connectionString = '';
 
 const db = new Pool({ connectionString });
 
