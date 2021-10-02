e0292904130fd39a41e0134049ff7d04e26d6de1
rugieri <rugieri.ronaldo@gmail.com>
2021-10-01 22:40:55 -0300

    created token with JWT

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import ForbiddenError from '../models/errors/forbidden-error-model';
 import userRepository from '../repositories/user-repository';
+import JWT from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
 
 const authRoute = Router();
 
@@ -31,7 +33,17 @@ authRoute.post(
         username,
         password
       );
-      console.log(username, password);
+      if (!user) {
+        throw new ForbiddenError('invalid user and password!');
+      }
+
+      const jwtPayload = { username: user.username };
+      const jwtOptions = { subject: user?.uuid };
+      const secretKey = 'my_secret_key';
+
+      const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+
+      res.status(StatusCodes.OK).json({ token: jwt });
     } catch (error) {
       next(error);
     }
